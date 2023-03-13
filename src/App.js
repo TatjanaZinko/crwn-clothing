417800c94501f9ad1cff239e12ced885fbf039bb
Tatjana Zinko <ztvproweb@gmailcom>
2023-03-13 19:14:51 +0200

    Add routing and navigation

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
+import Home from './routes/home/home.component';
+import SignIn from './routes/sign-in/sign-in.component';
+
+const Shop = () => {
+  return (
+    <div>Shop</div>
+  )
+}
 
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats'
-    },
-     {
-      id: 2,
-      title: 'Hats'
-    },
-     {
-      id: 3,
-      title: 'Hats'
-    },
-     {
-      id: 4,
-      title: 'Hats'
-    },
-     {
-      id: 5,
-      title: 'Hats'
-    },
-  ]
 
   return (
-    <div className="categories-container">
-      <Directory categories={categories} />
-    </div>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='signIn' element={<SignIn />} />
+      </Route>
+    </Routes>      
   );
 }
 
